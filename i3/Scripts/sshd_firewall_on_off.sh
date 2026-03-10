d02a19731056249455138fdb9c2bb5bfe659af7b
AyslanBatista <ayslan.batista@proton.me>
2026-03-10 19:17:10 -0300

    X11, i3, Kitty, Picom, audacious

--- i3/Scripts/sshd_firewall_on_off.sh
+++ i3/Scripts/sshd_firewall_on_off.sh
@@ -7,28 +7,27 @@
 
 #Criar um arquivo com o nome .secrets onde contem
 #uma variavel com o nome PORTA_SSHD com o numero
-#da porta que sera usada no SSH e o Firewall 
+#da porta que sera usada no SSH e o Firewall
 
 #VARIAVEIS
 #source $HOME/.config/i3/Scripts/.secrets
 source /opt/.secrets
-declare -a FIREWALL_PORTA=(`firewall-cmd --list-all`)
+declare -a FIREWALL_PORTA=($(firewall-cmd --list-all))
 
-ativando_desativando_firewall()
-{
-        if [ "${FIREWALL_PORTA[17]}" == "${PORTA_SSHD}/tcp" ]; then
-                 firewall-cmd --permanent --remove-port=${PORTA_SSHD}/tcp
-                 firewall-cmd --reload
-                 systemctl stop sshd
-                 firewall-cmd --list-all
-                 systemctl status sshd
-        else
-                firewall-cmd --permanent --add-port=${PORTA_SSHD}/tcp
-                firewall-cmd --reload
-                systemctl start sshd
-                firewall-cmd --list-all
-                systemctl status sshd
+ativando_desativando_firewall() {
+    if [ "${FIREWALL_PORTA[18]}" == "${PORTA_SSHD}/tcp" ]; then
+        firewall-cmd --permanent --remove-port=${PORTA_SSHD}/tcp
+        firewall-cmd --reload
+        systemctl stop sshd
+        firewall-cmd --list-all
+        systemctl status sshd
+    else
+        firewall-cmd --permanent --add-port=${PORTA_SSHD}/tcp
+        firewall-cmd --reload
+        systemctl start sshd
+        firewall-cmd --list-all
+        systemctl status sshd
 
-        fi
+    fi
 }
 ativando_desativando_firewall
